52a821f2119a91795e81ca552f6a00eb349b002b
SangTranTamLy <sangchaubr089@gmail.com>
2026-03-25 23:27:51 +0700

    Fix GitHub Pages entry file

--- home.js
+++ home.js
@@ -1,41 +1,48 @@
-let tasks = []; // Mảng lưu trữ các công việc
+let tasks = [];
 
-// Hàm thêm công việc mới
 function addTask() {
-    const taskInput = document.getElementById('taskInput'); 
-    const value = taskInput.value.trim(); // Lấy giá trị từ input và loại bỏ khoảng trắng
+    const taskInput = document.getElementById("taskInput");
+    const value = taskInput.value.trim();
+
     if (value === "") {
-        alert('Vui lòng nhập sự kiện')
+        alert("Vui long nhap cong viec");
         return;
-    }; // Nếu giá trị rỗng, không thêm công việc
+    }
+
     tasks.push({
         text: value,
         completed: false
-    }); // Thêm công việc mới vào mảng
-    taskInput.value = ""; // Xóa nội dung trong input sau khi thêm công việc
-    renderTasks(); // Cập nhật giao diện hiển thị công việc
+    });
+
+    taskInput.value = "";
+    renderTasks();
 }
+
 function toggleTask(index) {
-    tasks[index].completed = !tasks[index].completed; // Đảo ngược trạng thái hoàn thành của công việc
-    renderTasks(); // Cập nhật giao diện hiển thị công việc
+    tasks[index].completed = !tasks[index].completed;
+    renderTasks();
 }
+
 function removeTask(index) {
-    tasks.splice(index, 1); // Xóa công việc khỏi mảng
-    renderTasks(); // Cập nhật giao diện hiển thị công việc
+    tasks.splice(index, 1);
+    renderTasks();
 }
+
 function renderTasks() {
-    const list = document.getElementById('taskList'); // Lấy phần tử ul để hiển thị danh sách công việc
-    list.innerHTML = ""; // Xóa nội dung hiện tại của danh sách
-    tasks.forEach((task, index) => {// Duyệt qua mảng công việc và tạo phần tử li cho mỗi công việc
-        const li = document.createElement('li'); // Tạo phần tử li
+    const list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
         li.innerHTML = `
             <span onclick="toggleTask(${index})" style="cursor:pointer;">
-                ${task.completed ? "☑️" : "⬜"}
+                ${task.completed ? "[x]" : "[ ]"}
             </span>
             <span style="margin-left:10px; ${task.completed ? "text-decoration: line-through; color: gray;" : ""}">
                 ${task.text}
             </span>
-            <button class="remove-btn" onclick="removeTask(${index})" style="margin-left:10px;">Xóa</button>`;
-            list.appendChild(li); // Thêm phần tử li vào danh sách
+            <button class="remove-btn" onclick="removeTask(${index})" style="margin-left:10px;">Xoa</button>
+        `;
+        list.appendChild(li);
     });
 }
